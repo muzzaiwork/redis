42a76548d8f90805cb4958d8f1fbf4ff72a18dfd
muzzaiwork <yonkum@naver.com>
2026-03-04 11:10:10 +0900

    docs: add Redis setting guide and BoardService flow explanation

--- redis-app/dummy_data.sql
+++ redis-app/dummy_data.sql
@@ -13,4 +13,4 @@ SELECT
     CONCAT('Title', LPAD(n, 7, '0')) AS title,  -- 'Title' 다음에 7자리 숫자로 구성된 제목 생성
     CONCAT('Content', LPAD(n, 7, '0')) AS content,  -- 'Content' 다음에 7자리 숫자로 구성된 내용 생성
     TIMESTAMP(DATE_SUB(NOW(), INTERVAL FLOOR(RAND() * 3650 + 1) DAY) + INTERVAL FLOOR(RAND() * 86400) SECOND) AS created_at -- 최근 10년 내의 임의의 날짜와 시간 생성
-FROM cte;
+FROM cte;s
--- redis-app/src/main/java/com/example/redis/service/BoardService.java
+++ redis-app/src/main/java/com/example/redis/service/BoardService.java
@@ -2,6 +2,7 @@ package com.example.redis.service;
 
 import com.example.redis.entity.Board;
 import com.example.redis.repository.BoardRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +18,16 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    /**
+     * @Cacheable: Cache Aside 전략으로 캐싱이 적용됩니다.
+     * 1. 레디스에 데이터가 있다면 메서드를 실행하지 않고 레디스의 데이터를 반환합니다.
+     * 2. 데이터가 없다면 메서드 내부 로직을 실행한 뒤 리턴 값을 레디스에 저장하고 반환합니다.
+     *
+     * - cacheNames: 캐시 이름 (설정된 CacheManager에서 사용)
+     * - key: Redis에 저장될 키의 이름 (SpEL 표현식 사용)
+     * - cacheManager: 사용할 CacheManager 빈의 이름
+     */
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
